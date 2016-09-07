db5ce13c3a8571de5999f39dda8c054cc3e1e988
linh truong <truongthingoclinh@gmail.com>
2016-09-07 19:15:28 +0800

    Register: Commit 1st time

--- dist/js/magic.js
+++ dist/js/magic.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-	
+
+	// Global
 	$('ul.tabs li').click(function(){
 		var tab_id = $(this).attr('data-tab');
 
@@ -7,7 +8,16 @@ $(document).ready(function(){
 		$('.tab-content').removeClass('current');
 
 		$(this).addClass('current');
-		$("#"+tab_id).addClass('current');		
+		$("#"+tab_id).addClass('current');	
+
+		
+
+
+		if ($('ul.tabs li').attr('data-tab') == tab_id) {
+		    $('ul.tabs li').addClass('current');
+		} else {
+		    $('ul.tabs li').removeClass('current');
+		}
 
 	})
 
@@ -21,6 +31,26 @@ $(document).ready(function(){
 		$(this).addClass('current');
 		$("."+features_id).addClass('current');		
 
-	})
+	});
+
+
+
+	$(".checkbox").addClass("unchecked");
+	
+	$(".checkbox").click(function(){
+	
+		if($(this).children("input").attr("checked")){
+			// uncheck
+			$(this).children("input").attr({checked: ""});
+			$(this).removeClass("checked");
+			$(this).addClass("unchecked");
+		}else{
+			// check
+			$(this).children("input").attr({checked: "checked"});
+			$(this).removeClass("unchecked");
+			$(this).addClass("checked");
+		}
+		
+	});
 
 })
